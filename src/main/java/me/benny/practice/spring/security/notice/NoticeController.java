3f61e66133202f8747afd4d6f23624537c865b3e
wisekim <lwisekiml@naver.com>
2024-01-29 21:41:45 +0900

    PasswordEncoder

--- src/main/java/me/benny/practice/spring/security/notice/NoticeController.java
+++ src/main/java/me/benny/practice/spring/security/notice/NoticeController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import me.benny.practice.spring.security.note.NoteRegisterDto;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,7 @@ import java.util.List;
 public class NoticeController {
 
     private final NoticeService noticeService;
+    private final PasswordEncoder passwordEncoder;
 
     /**
      * 공지사항 조회
