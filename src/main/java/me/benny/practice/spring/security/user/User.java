c7ddfeadf86bc29192a954cebe2397e4fe7c0bc5
wisekim <lwisekiml@naver.com>
2024-01-29 21:07:37 +0900

    노트 서비스 로직 구현하기

---
+++ src/main/java/me/benny/practice/spring/security/user/User.java
@@ -0,0 +1,68 @@
+package me.benny.practice.spring.security.user;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Collection;
+import java.util.Collections;
+
+@Entity
+@Table
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User implements UserDetails {
+
+    @GeneratedValue
+    @Id
+    private Long id;
+    private String username;
+    private String password;
+    private String authority;
+
+    public User(
+            String username,
+            String password,
+            String authority
+    ) {
+        this.username = username;
+        this.password = password;
+        this.authority = authority;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton((GrantedAuthority) () -> authority);
+    }
+
+    public Boolean isAdmin() {
+        return authority.equals("ROLE_ADMIN");
+    }
+
+    // 계정 상태를 직접적으로 관리하고 싶으면 아래 메소드 수정
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
